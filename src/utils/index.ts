9a734c0ca2838fc45d5ddeddb91c2f711f53a33d
jjtsou <itsourtis@gmail.com>
2023-02-27 11:09:16 +0200

    Adding utils

---
+++ src/utils/index.ts
@@ -0,0 +1,5 @@
+import isValidEmail from './isValidEmail';
+import normalizeData from './normalizeData';
+import getDomain from './getDomain';
+
+export { isValidEmail, normalizeData, getDomain };
---
+++ src/utils/normalizeData.ts
@@ -0,0 +1,15 @@
+import { Recipient, Recipients, RecipientsByDomain } from '../types/Recipient';
+import getDomain from './getDomain';
+
+const normalizeData = (recipients: Recipients): RecipientsByDomain =>
+  recipients.reduce((acc: RecipientsByDomain, recipient: Recipient) => {
+    const { email } = recipient;
+    const domain = getDomain(email);
+
+    if (!acc[domain]) acc[domain] = [];
+    acc[domain].push(recipient);
+
+    return acc;
+  }, {});
+
+export default normalizeData;

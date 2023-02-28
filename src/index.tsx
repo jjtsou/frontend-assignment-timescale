d1f08534b3da6418f9b9dc4c857a6882d93447f7
jjtsou <itsourtis@gmail.com>
2023-02-28 23:17:43 +0200

    deploy on netlify

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 
 const container = document.getElementById('root');
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
 root.render(
   <React.StrictMode>
